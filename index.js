2f036c4fe9b3f40d34c405e709bee120bb2c98f0
wuzax <pottier.ludovic85600@gmail.com>
2022-06-28 17:26:14 +0200

    Added Roles Buttons System

--- index.js
+++ index.js
@@ -11,18 +11,27 @@ client.on("messageCreate", message => {
         message.delete()
         if(!message.member.permissions.has("ADMINISTRATOR")) return message.reply({content: ":x: | Tu n'a pas la permissions"}).then((msg) => msg.delete(5000))
         let button = new MessageButton().setCustomId("verify").setLabel("Verifier").setStyle("SUCCESS").setEmoji("✅")
-        let buttons = new MessageActionRow()
+        let verifyButtons = new MessageActionRow()
             .addComponents([button])
         let embed = new MessageEmbed()
         .setTitle(`😀 Bienvenue sur ${message.guild.name}`)
         .setDescription("Ce serveur est le serveur communautaire de Wuzax\n\nPour vous verifiez veuillez cliquer sur le boutton `✅ Verifier` !")
         .setFooter("Wuzax | © 2022", message.guild.iconURL())
-        message.channel.send({embeds: [embed], components: [buttons]})
+        message.channel.send({embeds: [embed], components: [verifyButtons]})
+    }
+
+    if(message.content.startsWith("!roles")) {
+        let giveawayButton = new MessageButton().setLabel("Giveaway").setCustomId("giveaway").setStyle("PRIMARY").setEmoji("🎉")
+        let annonceButton = new MessageButton().setLabel("Annonce").setCustomId("annonce").setStyle("PRIMARY").setEmoji("📌")
+        let rolesButtons = new MessageActionRow()
+            .addComponents([giveawayButton, annonceButton])
+        message.channel.send({components: [rolesButtons]})
     }
 })
 
 client.on("interactionCreate", async interaction => {
     if(interaction.isButton()) {
+        //Verify Button
         if(interaction.customId === "verify") {
             if(interaction.member.roles.cache.some(role => role.id === "868564194235142147")) {
                 await interaction.reply({content: ":x: | Tu es deja vérifier !", ephemeral: true})
@@ -30,6 +39,27 @@ client.on("interactionCreate", async interaction => {
             } else {
                 await interaction.member.roles.add("868564194235142147")
                 await interaction.reply({content: ":white_check_mark: | Tu es désormais verifier !", ephemeral: true})
+            }
+        }
+        /*                  Roles Button                    */
+        //Giveaway
+        if(interaction.customId === "giveaway") {
+            if(interaction.member.roles.cache.some(role => role.id === "990563902343487528")) {
+                await interaction.member.roles.remove("990563902343487528")
+                await interaction.reply({content: "🎉 | Tu avais déjà le rôle notification des Giveaway, je te les donc enlever !", ephemeral: true})
+            } else {
+                await interaction.member.roles.add("990563902343487528")
+                await interaction.reply({content: "🎉 | Tu viens d'obtenir le rôle notification des Giveaway !", ephemeral: true})
+            }
+        }
+        //Annonce
+        if(interaction.customId === "annonce") {
+            if(interaction.member.roles.cache.some(role => role.id === "990564185291235359")) {
+                await interaction.member.roles.remove("990564185291235359")
+                await interaction.reply({content: "🎉 | Tu avais déjà le rôle notification des Annonces, je te les donc enlever !", ephemeral: true})
+            } else {
+                await interaction.member.roles.add("990564185291235359")
+                await interaction.reply({content: "🎉 | Tu viens d'obtenir le rôle notification des Annonces !", ephemeral: true})
             }
         }
     }
