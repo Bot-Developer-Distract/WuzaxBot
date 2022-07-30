360b413b60fde5abfc1e3a3409d1dee9387e77d5
wuzax <pottier.ludovic85600@gmail.com>
2022-07-30 20:42:59 +0200

    Fix ticket system

--- Buttons/Ticket/lock.js
+++ Buttons/Ticket/lock.js
@@ -9,6 +9,21 @@ module.exports = {
      */
     async execute(interaction) {
         const { guildId, options, channel, guild, member } = interaction;
+        const Embed = new EmbedBuilder();
+        const TicketSetup = await TicketSetupData.findOne({ GuildID: guild.id });
+        if (!TicketSetup) {
+            Embed
+                .setColor("Red")
+                .setDescription(`:x: There is no data in the database`);
+            return interaction.editReply({embeds: [Embed], ephemeral: true});
+        }
+
+        if (!member.roles.cache.find(r => r.id === TicketSetup.Handlers) || !member.permissions.has("ADMINISTRATOR")) {
+            Embed
+                .setColor("Red")
+                .setDescription(`:x: This command is for staff only!`);
+            return interaction.editReply({embeds: [Embed], ephemeral: true});
+        }
         DB.findOne({ChannelID: channel.id}, async (err, data) => {
             if (err) throw err;
             if (!data) {
